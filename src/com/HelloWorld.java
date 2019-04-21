74a9c0f14ae5318c3153221828e745b0aef325e4
Administrator <Administrator@Cc221>
2019-04-21 20:45:58 +0800

    add a sysout

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -3,5 +3,6 @@ package com;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!! 随意改一改，再改改");
+		System.out.println("yijingchangbu duo l ");
 	}
 }
